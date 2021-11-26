8e4a21c3193126d1bd099b73c56db3e12982c774
adamwoolhether <adamwoolhether@gmail.com>
2021-11-26 08:13:53 +0800

    hasing, open addressing and linked lists

--- 03_hashing/hashOpenAddresssing/hashOpenAddressing.go
+++ 03_hashing/hashOpenAddresssing/hashOpenAddressing.go
@@ -6,55 +6,63 @@ import (
 	"log"
 )
 
-type Bucket struct {
+type Entry struct {
 	key   string
 	value int
 }
 
 type Hashtable struct {
-	table map[int]Bucket
-	size  int
-	entry int
+	table map[int]Entry
+	max   int
+	num   int
 }
 
 func NewHashtable(numObjects int) *Hashtable {
 	return &Hashtable{
 
-		table: make(map[int]Bucket, numObjects),
-		size:  numObjects,
-		entry: 0,
+		table: make(map[int]Entry, numObjects),
+		max:   numObjects,
+		num:   0,
 	}
 }
 
 func (h *Hashtable) get(k string) int {
-	hashCode := hash(k) % h.size
+	hashCode := hash(k) % h.max
 	for _, bucket := range h.table {
 		if bucket.key == k {
 			return bucket.value
 		}
-		hashCode = (hashCode + 1) % h.size
+		hashCode = (hashCode + 1) % h.max
 	}
 
 	return 0
 }
 
 func (h *Hashtable) put(k string, v int) {
-	hashCode := hash(k) % h.size
+	hashCode := hash(k) % h.max
 
-	for _, bucket := range h.table {
-		if bucket.key == k {
-			bucket.value = v
+	_, ok := h.table[hashCode]
+	if !ok {
+		h.table[hashCode] = Entry{key: k, value: v}
+		h.num++
+		return
+	}
+
+	for i, entry := range h.table {
+		if entry.key == k {
+			entry.value = v
 			return
 		}
-		hashCode = (hashCode + 1) % h.size
+
+		hashCode = i+1
 	}
 
-	if h.entry >= h.size-1 {
-		log.Fatalln("table is full")
+	if h.num >= h.max-1 {
+		log.Fatal("table is full")
 	}
 
-	h.table[hashCode] = Bucket{key: k, value: v}
-	h.entry++
+	h.table[hashCode] = Entry{key: k, value: v}
+	h.num++
 }
 
 func hash(s string) int {
@@ -64,11 +72,14 @@ func hash(s string) int {
 }
 
 func main() {
-	table := NewHashtable(1000)
+	table := NewHashtable(3)
+
 
 	table.put("April", 30)
 	table.put("May", 31)
 	table.put("September", 30)
+
+	fmt.Println(table)
 
 	fmt.Println(table.get("August"))
 	fmt.Println(table.get("September"))
--- 03_hashing/hashingLinkedList/hashingLinkedList.go
+++ 03_hashing/hashingLinkedList/hashingLinkedList.go
@@ -9,29 +9,27 @@ import (
 Separate chaining implementation w/ linked lists.
 */
 
-type LinkedBucket struct {
-	key string
+type linkedEntry struct {
+	key   string
 	value int
-	// next int
 }
 
 type Hashtable struct {
-	table map[int][]LinkedBucket
-	size  int
-	entry int
+	table map[int][]linkedEntry
+	max   int
+	num   int
 }
 
 func NewHashtable(numObjects int) *Hashtable {
 	return &Hashtable{
 
-		table: make(map[int][]LinkedBucket, numObjects),
-		size:  numObjects,
-		entry: 0,
+		table: make(map[int][]linkedEntry, numObjects),
+		max:   numObjects,
 	}
 }
 
 func (h *Hashtable) get(k string) int {
-	hashCode := hash(k) % h.size
+	hashCode := hash(k) % h.max
 
 	table := h.table[hashCode]
 
@@ -45,23 +43,23 @@ func (h *Hashtable) get(k string) int {
 }
 
 func (h *Hashtable) put(k string, v int) {
-	hashCode := hash(k) % h.size
+	hashCode := hash(k) % h.max
 
-	if len(h.table[hashCode]) < 1 {
-		h.table[hashCode] = make([]LinkedBucket, h.size)
+	entry, ok := h.table[hashCode]
+	if !ok {
+		h.table[hashCode] = make([]linkedEntry, 1, h.max)
+		h.table[hashCode][h.num] = linkedEntry{key: k, value: v}
+		return
 	}
 
-	table := h.table[hashCode]
-
-	for _, bucket := range table {
+	for _, bucket := range entry {
 		if bucket.key == k {
 			bucket.value = v
 			return
 		}
 	}
 
-	h.table[hashCode][h.entry] = LinkedBucket{key: k, value: v}
-	h.entry++
+	h.table[hashCode] = append([]linkedEntry{{key: k, value: v}}, h.table[hashCode]...)
 }
 
 func hash(s string) int {
@@ -70,13 +68,19 @@ func hash(s string) int {
 	return int(h.Sum32())
 }
 
+
 func main() {
-	table := NewHashtable(1000)
+	table := NewHashtable(10)
 
 	table.put("April", 30)
 	table.put("May", 31)
 	table.put("September", 30)
+	table.put("RANDY", 33)
+	table.put("ERIC", 30)
 
 	fmt.Println(table.get("August"))
 	fmt.Println(table.get("September"))
+	fmt.Println(table.get("RANDY"))
+	fmt.Println(table)
+
 }
