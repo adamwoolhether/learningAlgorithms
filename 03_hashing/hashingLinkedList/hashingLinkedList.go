f9a8474f41f49ef20cb38344ca5fb82b1f914002
adamwoolhether <adamwoolhether@gmail.com>
2021-11-29 15:19:53 +0800

    fixex hashedmap linkedlist

--- 03_hashing/hashingLinkedList/hashingLinkedList.go
+++ 03_hashing/hashingLinkedList/hashingLinkedList.go
@@ -12,10 +12,11 @@ Separate chaining implementation w/ linked lists.
 type linkedEntry struct {
 	key   string
 	value int
+	next  *linkedEntry
 }
 
 type Hashtable struct {
-	table map[int][]linkedEntry
+	table map[int]*linkedEntry
 	max   int
 	num   int
 }
@@ -23,71 +24,75 @@ type Hashtable struct {
 func NewHashtable(numObjects int) *Hashtable {
 	return &Hashtable{
 
-		table: make(map[int][]linkedEntry, numObjects),
+		table: make(map[int]*linkedEntry, numObjects),
 		max:   numObjects,
-		num: 0,
+		num:   0,
 	}
 }
 
 func (h *Hashtable) get(k string) int {
 	hashCode := hash(k) % h.max
 
-	table := h.table[hashCode]
-
-	for _, bucket := range table {
-		if bucket.key == k {
-			return bucket.value
-		}
+	entry, ok := h.table[hashCode]
+	if !ok {
+		return -1
 	}
 
-	return 0
+	if entry.key == k {
+		return entry.value
+	}
+
+	for entry != nil {
+		if entry.key == k {
+			return entry.value
+		}
+		entry = entry.next
+	}
+
+	return -1
 }
 
 func (h *Hashtable) put(k string, v int) {
 	hashCode := hash(k) % h.max
 
-	fmt.Println(k, hashCode)
-
 	entry, ok := h.table[hashCode]
 	if !ok {
-		h.table[hashCode] = make([]linkedEntry, 1, h.max)
-		h.table[hashCode][0] = linkedEntry{key: k, value: v}
+		h.table[hashCode] = &linkedEntry{key: k, value: v, next: nil}
 		h.num++
 		return
 	}
 
-	for _, bucket := range entry {
-		if bucket.key == k {
-			bucket.value = v
-			return
-		}
-	}
-
-	h.table[hashCode] = append([]linkedEntry{{key: k, value: v}}, h.table[hashCode]...)
-	// h.num++
+	h.table[hashCode] = &linkedEntry{key: k, value: v, next: entry}
 }
 
 func (h *Hashtable) remove(k string) {
 	hashCode := hash(k) % h.max
 
-	fmt.Println(k, hashCode)
-
 	entry, ok := h.table[hashCode]
 	if !ok {
-		fmt.Println("not found")
-	}
-
-	if len(entry) <2 {
-		delete(h.table, hashCode)
-		h.num--
 		return
 	}
 
-	for i, bucket := range entry {
-		if bucket.key == k {
-			h.table[hashCode] = append(h.table[hashCode][:i], h.table[hashCode][i+1:]...)
+	if entry.key == k {
+		if entry.next == nil {
+			delete(h.table, hashCode)
+			h.num--
 			return
 		}
+		h.table[hashCode] = entry.next
+		return
+	}
+
+	for entry.next != nil {
+		if entry.next.key == k {
+			if entry.next.next != nil {
+				h.table[hashCode].next = entry.next.next
+				return
+			}
+			h.table[hashCode].next = &linkedEntry{}
+			return
+		}
+		entry = entry.next
 	}
 }
 
@@ -98,19 +103,34 @@ func hash(s string) int {
 }
 
 func main() {
-	table := NewHashtable(5)
+	table := NewHashtable(10)
 
+	table.put("January", 31)
+	table.put("February", 28)
+	table.put("March", 31)
 	table.put("April", 30)
 	table.put("May", 31)
+	table.put("June", 30)
+	table.put("July", 31)
+	table.put("August", 31)
 	table.put("September", 30)
-	table.put("RANDY", 33)
-	table.put("ERIC", 30)
+	table.put("October", 31)
+	table.put("November", 30)
+	table.put("December", 31)
 
 	fmt.Println(table.get("August"))
 	fmt.Println(table.get("September"))
-	fmt.Println(table.get("RANDY"))
+	fmt.Println(table.get("October"))
+	fmt.Println(table.get("April"))
+	fmt.Println(table.get("May"))
 	fmt.Println(table)
 
+	table.remove("October")
 	table.remove("May")
+	table.remove("September")
+
+	fmt.Println(table.get("May"))
+	fmt.Println(table.get("September"))
+	fmt.Println(table.get("October"))
 	fmt.Println(table)
 }
